ba5b0193c44f054d355fb4dff197f92170415e5a
312Evan <emcgrael@outlook.com>
2026-01-12 10:18:27 -0500

    base robot code for 2026 season

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.utility.IO;
+import frc.robot.utility.AutomatedController;
+import frc.robot.utility.Util;
+import frc.robot.commands.DefaultDrive;
+
+public class RobotContainer {
+  public IO io = new IO();
+  public final AutomatedController main;
+
+  private final SendableChooser<Command> auto_selector;
+  Command current_auto = new PrintCommand("");
+  final SendableChooser<Integer> driver_selector = new SendableChooser<Integer>();
+
+  public RobotContainer() {
+    main = new AutomatedController(0, io);
+
+    configureAuton();
+
+    auto_selector = AutoBuilder.buildAutoChooser();
+    auto_selector.onChange((command) -> {current_auto = command;});
+
+    SmartDashboard.putData("Autos", auto_selector);
+    SmartDashboard.putData("Run Test Auto", Util.Do(current_auto::schedule));
+
+    SmartDashboard.putData("Main-Controller Mode", main.selector);
+
+    SmartDashboard.putData("Driver", driver_selector);
+    io.chassis.setDefaultCommand(new DefaultDrive(io, main.controller));
+  }
+
+  public void configureAuton(){
+    // Create auton commands
+  }
+
+  public Command getAutonomousCommand() {
+    return auto_selector.getSelected();
+  }
+}
