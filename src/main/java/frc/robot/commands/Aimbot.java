64f0d04dcea16cc1c518500e10820df6759f9572
312Evan <emcgrael@outlook.com>
2026-03-02 10:01:24 -0500

    aimbot

--- src/main/java/frc/robot/commands/Aimbot.java
+++ src/main/java/frc/robot/commands/Aimbot.java
@@ -13,11 +13,11 @@ import frc.robot.utility.IO;
 public class Aimbot extends Command {
   IO io;
   swerveState state;
-  PIDController pid = new PIDController(0.01, 0, 0.01); // TODO tune this
+  PIDController pid = new PIDController(0.0001, 0, 0.00); // TODO tune this
   boolean blue;
   double target;
   boolean auton = false;
-  private static final double ANGLE_OFFSET = Math.toRadians(180);
+  private static final double ANGLE_OFFSET = Math.toRadians(0);
 
   public Aimbot(IO io, swerveState state) {
     this.io = io;
@@ -34,38 +34,25 @@ public class Aimbot extends Command {
   public void initialize() {
     io.chassis.currentState = state;
     blue = DriverStation.getAlliance().get() == Alliance.Blue;
-    pid.setTolerance(Math.toRadians(5));
+    pid.setTolerance(Math.toRadians(7));
     pid.enableContinuousInput(-Math.PI, Math.PI);
     pid.reset();
   }
 
   @Override
   public void execute() {
+    // Math.atan2 returns a range of -pi to pi, this normalizes the estimated rotation.
+    double rotation = Math.toRadians(io.chassis.getEstimatedRotation()) - Math.PI;
+
     if (state == swerveState.SCORING) {
       Translation2d hub = blue ? RobotContainer.BLUE_HUB : RobotContainer.RED_HUB;
       Translation2d diff = hub.minus(io.chassis.getEstimatedPose().getTranslation());
-      target = Math.atan2(diff.getY(), diff.getX()) + Math.toRadians(ANGLE_OFFSET);
+      target = Math.atan2(diff.getY(), diff.getX()) + ANGLE_OFFSET;
 
       SmartDashboard.putNumber("Target", Math.toDegrees(target));
 
-      if (Math.abs((Math.toRadians(io.chassis.getEstimatedRotation())) - target) < Math.toRadians(7)) {
-        io.chassis.targetRotation = 0.0;
-      } else {
-        if (target - Math.toRadians(io.chassis.getEstimatedRotation()) > 0) {
-          io.chassis.targetRotation = -0.025;
-        } else {
-          io.chassis.targetRotation = 0.025;
-        }
-      }
-
-      // TODO find out why PID loop isn't working
-      // if(pid.atSetpoint()){
-      // io.chassis.targetRotation = 0.0;
-      // }
-      // else{
-      // io.chassis.targetRotation =
-      // pid.calculate(Math.toRadians(io.chassis.getEstimatedRotation()), target);
-      // }
+      io.chassis.targetRotation = pid.atSetpoint() ? 0.0
+          : pid.calculate(Math.toRadians(rotation), target);
     }
   }
 
@@ -76,7 +63,7 @@ public class Aimbot extends Command {
 
   @Override
   public boolean isFinished() {
-    return auton ? (Math.abs((Math.toRadians(io.chassis.getEstimatedRotation())) - target) < Math.toRadians(3))
+    return auton ? pid.atSetpoint()
         : (io.chassis.currentState != state);
   }
 }
